7b5e9f52883495f527da67ed292eac8c897d3762
Charris1998 <ch50010759@gmail.com>
2022-04-02 22:20:07 -0400

    added function calc perimeter

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function hoursIntoSeconds(hours) {
     return hours * 60 * 60
 }
 console.log(hoursIntoSeconds(2))
+
+function calcPerimeter(length, width) {
+    return 2 * length + 2 * width
+}
+console.log(calcPerimeter(6, 7))
