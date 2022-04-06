a5f57f6e547189b914d07b95bdd66c2c3754d106
Charris1998 <ch50010759@gmail.com>
2022-04-06 09:20:27 -0400

    added function arr length

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ function filterOutFalsy(elem1, elem2) {
     return !elem1 ? elem1 : elem2
 }
 console.log(filterOutFalsy(100, 0))
+
+
+// Return the length of any given array 
+// - Given an array, return its length
+
+function arrLength(arr) {
+return arr.length
+}
+console.log(arrLength([1, 2, 3, 4]))
